97d5c76d7d1b5a923b764482eb91b75ed9b631af
Tokuma Abdisa <106821330+tokukiyu@users.noreply.github.com>
2023-04-03 00:19:04 +0300

    master react

---
+++ masterReact/src/App.jsx
@@ -0,0 +1,11 @@
+import Message from "./message";
+import ListGroup from "./components/ListGroup"
+
+function App() {
+  return (
+    <div>
+      <ListGroup />
+    </div>
+  );
+}
+export default App;
---
+++ masterReact/src/message.jsx
@@ -0,0 +1,6 @@
+function Message() {
+  const name = "";
+  if (name) return <h1>hello {name}</h1>;
+  else return <h1>hello world</h1>;
+}
+export default Message;
